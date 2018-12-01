93988c6b941f2da0b2ac0775bd3f1087ea1eff4f
jbnewlin <jnewlin@purdue.edu>
2018-12-01 11:46:48 -0500

    Adding some things slowly

---
+++ src/components/ApplicationHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <nav class="navbar navbar-dark bg-dark">
+      <a class="navbar-brand mb-0 h1" href="#">
+        <img src="../../public/d.svg" width="30" height="30" alt="">
+        dinnertable
+      </a>
+      <div class="navbar-nav">
+        <a class="nav-link" href="#">
+          Login
+        </a>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ApplicationHeader'
+}
+</script>
+
+<style scoped>
+</style>
